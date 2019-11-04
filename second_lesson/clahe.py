c75dfe0c2d0963ffac224229a5068156541e9aaf
am502 <antegzeperob@gmail.com>
2019-11-04 16:25:35 +0300

    fix clahe

--- second_lesson/clahe.py
+++ second_lesson/clahe.py
@@ -1,110 +1,103 @@
 import cv2
-import matplotlib.pyplot as plt
 import numpy as np
 
-orig_image = cv2.imread('image20.png')
-gray_image = cv2.cvtColor(orig_image, cv2.COLOR_BGR2GRAY)
-height, width = gray_image.shape
 
-clip_limit = 80
-bins_quantity = 256
-pixels_quantity = height * width
-
-n = 8
-m = 8
-
-block_height = int(height / n)
-block_width = int(width / m)
-
-block_pixels_quantity = block_height * block_width
+def up_round(number):
+    rounded = int(number)
+    if rounded == number:
+        return rounded
+    else:
+        return rounded + 1
 
 
-def get_block(image, i, j):
+def get_block_with_size_by_indexes(matrix, block_row_index, block_column_index, block_height, block_width):
+    height, width = matrix.shape
+    start_row_index = block_row_index * block_height
+    start_column_index = block_column_index * block_width
+    end_row_index = start_row_index + block_height
+    end_column_index = start_column_index + block_width
+    if end_row_index > height:
+        end_row_index = height
+    if end_column_index > width:
+        end_column_index = width
     block = np.zeros((block_height, block_width), dtype=int)
-    row_index = i * block_height
-    column_index = j * block_width
-    for i in range(row_index, row_index + block_height):
-        for j in range(column_index, column_index + block_width):
-            block[i - row_index][j - column_index] = image[i][j]
-    return block
+    for i in range(start_row_index, end_row_index):
+        for j in range(start_column_index, end_column_index):
+            block[i - start_row_index][j - start_column_index] = matrix[i][j]
+    return block, end_row_index - start_row_index, end_column_index - start_column_index
 
 
-def calculate_histogram(image, h, w):
-    x = np.zeros(bins_quantity, dtype=int)
-    for i in range(h):
-        for j in range(w):
-            x[image[i][j]] += 1
-    return x
+def calculate_histogram(matrix, bins_count):
+    height, width = matrix.shape
+    histogram = np.zeros(bins_count, dtype=int)
+    for i in range(height):
+        for j in range(width):
+            histogram[matrix[i][j]] += 1
+    return histogram
 
 
-def clip_histogram(x):
-    pixels_to_distribute = 0
-    for i in range(len(x)):
-        if x[i] > clip_limit:
-            pixels_to_distribute += (x[i] - clip_limit)
-            x[i] = clip_limit
-    part = int(pixels_to_distribute / bins_quantity)
-    for i in range(len(x)):
-        x[i] += part
-    return x
+def normalize_histogram(histogram, pixels_count):
+    normalized_histogram = np.zeros(len(histogram), dtype=float)
+    for i, e in enumerate(histogram):
+        normalized_histogram[i] = e / pixels_count
+    return normalized_histogram
 
 
-def normalize_histogram(x):
-    x_normalized = [i / max(x) for i in x]
-    cdf = np.zeros(bins_quantity, dtype=float)
+def clip_histogram(normalized_histogram, clip_limit):
+    redistribution = 0
+    for i, e in enumerate(normalized_histogram):
+        if e > clip_limit:
+            redistribution += (e - clip_limit)
+            normalized_histogram[i] = clip_limit
+    part = redistribution / len(normalized_histogram)
+    for i, e in enumerate(normalized_histogram):
+        normalized_histogram[i] += part
+    return normalized_histogram
+
+
+def calculate_cdf(normalized_histogram):
+    cdf = np.zeros(len(normalized_histogram), dtype=float)
     previous_sum = 0
-    for i, e in enumerate(x):
-        previous_sum += (e / pixels_quantity)
-        cdf[i] = previous_sum
-    return cdf, x_normalized
-
-
-def calculate_cdf(x):
-    cdf = np.zeros(bins_quantity, dtype=float)
-    previous_sum = 0
-    for i, e in enumerate(x):
-        previous_sum += (e / block_pixels_quantity)
+    for i, e in enumerate(normalized_histogram):
+        previous_sum += e
         cdf[i] = previous_sum
     return cdf
 
 
-def get_block_indexes_of_pixel(i, j):
-    block_row_index = int(i / block_height)
-    block_column_index = int(j / block_width)
+def get_block_indexes_by_pixel_indexes(pixel_row_index, pixel_column_index, block_height, block_width):
+    block_row_index = int(pixel_row_index / block_height)
+    block_column_index = int(pixel_column_index / block_width)
     return block_row_index, block_column_index
 
 
-def get_center_indexes_of_block(i, j):
-    center_row_index = (i + 1) * block_height - block_height / 2
-    center_column_index = (j + 1) * block_width - block_width / 2
-    return int(center_row_index), int(center_column_index)
+def transform(matrix, cdfs, block, pixel_i, pixel_j, bins_count):
+    return round(cdfs[block[0]][block[1]][matrix[pixel_i][pixel_j]] * (bins_count - 1))
 
 
-def transform(image, cdf, block_i, block_j, pixel_i, pixel_j):
-    return round(cdf[block_i][block_j][image[pixel_i][pixel_j]] * (bins_quantity - 1))
+def transform_linear(image, cdfs, centers, blocks, pixel_i, pixel_j, bins_count):
+    first = transform(image, cdfs, blocks[0], pixel_i, pixel_j, bins_count)
+    second = transform(image, cdfs, blocks[1], pixel_i, pixel_j, bins_count)
 
+    f_c_i, f_c_j = centers[blocks[0][0]][blocks[0][1]]
+    s_c_i, s_c_j = centers[blocks[1][0]][blocks[1][1]]
 
-def transform_linear(image, cdf, blocks, pixel_i, pixel_j):
-    left = transform(image, cdf, blocks[0][0], blocks[0][1], pixel_i, pixel_j)
-    right = transform(image, cdf, blocks[1][0], blocks[1][1], pixel_i, pixel_j)
-
-    l_i, l_j = get_center_indexes_of_block(blocks[0][0], blocks[0][1])
-    r_i, r_j = get_center_indexes_of_block(blocks[1][0], blocks[1][1])
-
-    if r_j - l_j == 0:
-        return left * (r_i - pixel_i) / (r_i - l_i) + right * (pixel_i - l_i) / (r_i - l_i)
+    if s_c_j - f_c_j == 0:
+        return first * (s_c_i - pixel_i) / (s_c_i - f_c_i) + second * (pixel_i - f_c_i) / (s_c_i - f_c_i)
     else:
-        return left * (r_j - pixel_j) / (r_j - l_j) + right * (pixel_j - l_j) / (r_j - l_j)
+        return first * (s_c_j - pixel_j) / (s_c_j - f_c_j) + second * (pixel_j - f_c_j) / (s_c_j - f_c_j)
 
 
-def transform_bilinear(image, cdf, blocks, y, x):
-    l_u = transform(image, cdf, blocks[0][0], blocks[0][1], y, x)
-    r_u = transform(image, cdf, blocks[1][0], blocks[1][1], y, x)
-    l_d = transform(image, cdf, blocks[2][0], blocks[2][1], y, x)
-    r_d = transform(image, cdf, blocks[3][0], blocks[3][1], y, x)
+def transform_bilinear(image, cdfs, centers, blocks, pixel_i, pixel_j, bins_count):
+    x = pixel_j
+    y = pixel_i
 
-    y1, x1 = get_center_indexes_of_block(blocks[0][0], blocks[0][1])
-    y2, x2 = get_center_indexes_of_block(blocks[3][0], blocks[3][1])
+    l_u = transform(image, cdfs, blocks[0], pixel_i, pixel_j, bins_count)
+    r_u = transform(image, cdfs, blocks[1], pixel_i, pixel_j, bins_count)
+    l_d = transform(image, cdfs, blocks[2], pixel_i, pixel_j, bins_count)
+    r_d = transform(image, cdfs, blocks[3], pixel_i, pixel_j, bins_count)
+
+    y1, x1 = centers[blocks[0][0]][blocks[0][1]]
+    y2, x2 = centers[blocks[3][0]][blocks[3][1]]
 
     i1 = l_u * (x2 - x) / (x2 - x1) + r_u * (x - x1) / (x2 - x1)
     i2 = l_d * (x2 - x) / (x2 - x1) + r_d * (x - x1) / (x2 - x1)
@@ -112,93 +105,113 @@ def transform_bilinear(image, cdf, blocks, y, x):
     return i1 * (y2 - y) / (y2 - y1) + i2 * (y - y1) / (y2 - y1)
 
 
-def main():
-    cdf = np.ndarray((n, m, bins_quantity))
-    for i in range(n):
-        for j in range(m):
-            current_block = get_block(gray_image, i, j)
-            cdf[i][j] = calculate_cdf(calculate_histogram(current_block, block_height, block_width))
-
-    result = gray_image.copy()
-    for i in range(height):
-        for j in range(width):
-            b_r_index, b_c_index = get_block_indexes_of_pixel(i, j)
-            c_r_index, c_c_index = get_center_indexes_of_block(b_r_index, b_c_index)
-
-            if i <= c_r_index and j <= c_c_index:
-                if b_r_index == 0 and b_c_index == 0:
-                    new_pixel_value = transform(result, cdf, b_r_index, b_c_index, i, j)
-                elif b_r_index == 0:
-                    blocks = np.array([[b_r_index, b_c_index - 1], [b_r_index, b_c_index]])
-                    new_pixel_value = transform_linear(result, cdf, blocks, i, j)
-                elif b_c_index == 0:
-                    blocks = np.array([[b_r_index - 1, b_c_index], [b_r_index, b_c_index]])
-                    new_pixel_value = transform_linear(result, cdf, blocks, i, j)
-                else:
-                    blocks = np.array([[b_r_index - 1, b_c_index - 1], [b_r_index - 1, b_c_index],
-                                       [b_r_index, b_c_index - 1], [b_r_index, b_c_index]])
-                    new_pixel_value = transform_bilinear(result, cdf, blocks, i, j)
-            elif i <= c_r_index and j >= c_c_index:
-                if b_r_index == 0 and b_c_index == m - 1:
-                    new_pixel_value = transform(result, cdf, b_r_index, b_c_index, i, j)
-                elif b_r_index == 0:
-                    blocks = np.array([[b_r_index, b_c_index], [b_r_index, b_c_index + 1]])
-                    new_pixel_value = transform_linear(result, cdf, blocks, i, j)
-                elif b_c_index == m - 1:
-                    blocks = np.array([[b_r_index - 1, b_c_index], [b_r_index, b_c_index]])
-                    new_pixel_value = transform_linear(result, cdf, blocks, i, j)
-                else:
-                    blocks = np.array([[b_r_index - 1, b_c_index], [b_r_index - 1, b_c_index + 1],
-                                       [b_r_index, b_c_index], [b_r_index, b_c_index + 1]])
-                    new_pixel_value = transform_bilinear(result, cdf, blocks, i, j)
-            elif i >= c_r_index and j <= c_c_index:
-                if b_r_index == n - 1 and b_c_index == 0:
-                    new_pixel_value = transform(result, cdf, b_r_index, b_c_index, i, j)
-                elif b_r_index == n - 1:
-                    blocks = np.array([[b_r_index, b_c_index - 1], [b_r_index, b_c_index]])
-                    new_pixel_value = transform_linear(result, cdf, blocks, i, j)
-                elif b_c_index == 0:
-                    blocks = np.array([[b_r_index, b_c_index], [b_r_index + 1, b_c_index]])
-                    new_pixel_value = transform_linear(result, cdf, blocks, i, j)
-                else:
-                    blocks = np.array([[b_r_index, b_c_index - 1], [b_r_index, b_c_index],
-                                       [b_r_index + 1, b_c_index - 1], [b_r_index + 1, b_c_index]])
-                    new_pixel_value = transform_bilinear(result, cdf, blocks, i, j)
-            elif i >= c_r_index and j >= c_c_index:
-                if b_r_index == n - 1 and b_c_index == m - 1:
-                    new_pixel_value = transform(result, cdf, b_r_index, b_c_index, i, j)
-                elif b_r_index == n - 1:
-                    blocks = np.array([[b_r_index, b_c_index], [b_r_index, b_c_index + 1]])
-                    new_pixel_value = transform_linear(result, cdf, blocks, i, j)
-                elif b_c_index == m - 1:
-                    blocks = np.array([[b_r_index, b_c_index], [b_r_index + 1, b_c_index]])
-                    new_pixel_value = transform_linear(result, cdf, blocks, i, j)
-                else:
-                    blocks = np.array([[b_r_index, b_c_index], [b_r_index, b_c_index + 1],
-                                       [b_r_index + 1, b_c_index], [b_r_index + 1, b_c_index + 1]])
-                    new_pixel_value = transform_bilinear(result, cdf, blocks, i, j)
-            else:
-                new_pixel_value = transform(result, cdf, b_r_index, b_c_index, i, j)
-
-            result[i][j] = new_pixel_value
-
-    x = calculate_histogram(result, height, width)
-    result_cdf, x_normalized = normalize_histogram(x)
-    plt.bar(np.arange(bins_quantity), x_normalized)
-    plt.plot(result_cdf, 'r')
-    plt.show()
-
-    cv2.namedWindow('Source', cv2.WINDOW_NORMAL)
-    cv2.imshow('Source', gray_image)
-
-    cv2.namedWindow('Result', cv2.WINDOW_NORMAL)
-    cv2.imshow('Result', result)
-
+def show_image(*images):
+    for i, image in enumerate(images):
+        cv2.namedWindow(str(i), cv2.WINDOW_NORMAL)
+        cv2.imshow(str(i), image)
     while True:
-        k = cv2.waitKey(0)
-        if k == 27:
+        key = cv2.waitKey(0)
+        if key == 27:
             cv2.destroyAllWindows()
             break
+
+
+def main():
+    original_image = cv2.imread('image20.png')
+    grayscale_image = cv2.cvtColor(original_image, cv2.COLOR_BGR2GRAY)
+    height, width = grayscale_image.shape
+
+    clip_limit = 0.025
+    bins_count = 256
+    rows_count = 8
+    columns_count = 8
+
+    block_height = up_round(height / rows_count)
+    block_width = up_round(width / columns_count)
+
+    cdfs = np.ndarray((rows_count, columns_count, bins_count))
+    centers = np.ndarray((rows_count, columns_count, 2))
+    for i in range(rows_count):
+        for j in range(columns_count):
+            current_block, cur_block_h, cur_block_w = get_block_with_size_by_indexes(
+                grayscale_image, i, j, block_height, block_width)
+            histogram = calculate_histogram(current_block, bins_count)
+            normalized_histogram = normalize_histogram(histogram, cur_block_h * cur_block_w)
+            cdfs[i][j] = calculate_cdf(clip_histogram(normalized_histogram, clip_limit))
+
+            center_row_index = (i + 1) * cur_block_h - cur_block_h / 2
+            center_column_index = (j + 1) * cur_block_w - cur_block_w / 2
+            centers[i][j] = [int(center_row_index), int(center_column_index)]
+
+    processed_image = grayscale_image.copy()
+    for i in range(height):
+        for j in range(width):
+            block_i, block_j = get_block_indexes_by_pixel_indexes(i, j, block_height, block_width)
+            center_i, center_j = centers[block_i][block_j]
+
+            if i <= center_i and j <= center_j:
+                if block_i == 0 and block_j == 0:
+                    block = np.array([block_i, block_j])
+                    new_pixel_value = transform(processed_image, cdfs, block, i, j, bins_count)
+                elif block_i == 0:
+                    blocks = np.array([[block_i, block_j - 1], [block_i, block_j]])
+                    new_pixel_value = transform_linear(processed_image, cdfs, centers, blocks, i, j, bins_count)
+                elif block_j == 0:
+                    blocks = np.array([[block_i - 1, block_j], [block_i, block_j]])
+                    new_pixel_value = transform_linear(processed_image, cdfs, centers, blocks, i, j, bins_count)
+                else:
+                    blocks = np.array([[block_i - 1, block_j - 1], [block_i - 1, block_j],
+                                       [block_i, block_j - 1], [block_i, block_j]])
+                    new_pixel_value = transform_bilinear(processed_image, cdfs, centers, blocks, i, j, bins_count)
+            elif i <= center_i and j >= center_j:
+                if block_i == 0 and block_j == columns_count - 1:
+                    block = np.array([block_i, block_j])
+                    new_pixel_value = transform(processed_image, cdfs, block, i, j, bins_count)
+                elif block_i == 0:
+                    blocks = np.array([[block_i, block_j], [block_i, block_j + 1]])
+                    new_pixel_value = transform_linear(processed_image, cdfs, centers, blocks, i, j, bins_count)
+                elif block_j == columns_count - 1:
+                    blocks = np.array([[block_i - 1, block_j], [block_i, block_j]])
+                    new_pixel_value = transform_linear(processed_image, cdfs, centers, blocks, i, j, bins_count)
+                else:
+                    blocks = np.array([[block_i - 1, block_j], [block_i - 1, block_j + 1],
+                                       [block_i, block_j], [block_i, block_j + 1]])
+                    new_pixel_value = transform_bilinear(processed_image, cdfs, centers, blocks, i, j, bins_count)
+            elif i >= center_i and j <= center_j:
+                if block_i == rows_count - 1 and block_j == 0:
+                    block = np.array([block_i, block_j])
+                    new_pixel_value = transform(processed_image, cdfs, block, i, j, bins_count)
+                elif block_i == rows_count - 1:
+                    blocks = np.array([[block_i, block_j - 1], [block_i, block_j]])
+                    new_pixel_value = transform_linear(processed_image, cdfs, centers, blocks, i, j, bins_count)
+                elif block_j == 0:
+                    blocks = np.array([[block_i, block_j], [block_i + 1, block_j]])
+                    new_pixel_value = transform_linear(processed_image, cdfs, centers, blocks, i, j, bins_count)
+                else:
+                    blocks = np.array([[block_i, block_j - 1], [block_i, block_j],
+                                       [block_i + 1, block_j - 1], [block_i + 1, block_j]])
+                    new_pixel_value = transform_bilinear(processed_image, cdfs, centers, blocks, i, j, bins_count)
+            elif i >= center_i and j >= center_j:
+                if block_i == rows_count - 1 and block_j == columns_count - 1:
+                    block = np.array([block_i, block_j])
+                    new_pixel_value = transform(processed_image, cdfs, block, i, j, bins_count)
+                elif block_i == rows_count - 1:
+                    blocks = np.array([[block_i, block_j], [block_i, block_j + 1]])
+                    new_pixel_value = transform_linear(processed_image, cdfs, centers, blocks, i, j, bins_count)
+                elif block_j == columns_count - 1:
+                    blocks = np.array([[block_i, block_j], [block_i + 1, block_j]])
+                    new_pixel_value = transform_linear(processed_image, cdfs, centers, blocks, i, j, bins_count)
+                else:
+                    blocks = np.array([[block_i, block_j], [block_i, block_j + 1],
+                                       [block_i + 1, block_j], [block_i + 1, block_j + 1]])
+                    new_pixel_value = transform_bilinear(processed_image, cdfs, centers, blocks, i, j, bins_count)
+            else:
+                block = np.array([block_i, block_j])
+                new_pixel_value = transform(processed_image, cdfs, block, i, j, bins_count)
+
+            processed_image[i][j] = new_pixel_value
+
+    show_image(original_image, processed_image)
 
 
 main()
--- third_lesson/sauvola.py
+++ third_lesson/sauvola.py
@@ -1,38 +1,103 @@
+import math
+
 import cv2
-import matplotlib.pyplot as plt
 import numpy as np
 
 
-def get_summed_area_table(image):
+def calculate_s(image):
     height, width = image.shape
-    s = np.zeros((height, width))
+    s1 = np.zeros((height, width))
+    s2 = np.zeros((height, width))
     for i in range(height):
         for j in range(width):
-            s_x = 0
-            s_x_y = 0
-            s_y = 0
+            s1_x, s2_x = 0, 0
+            s1_y, s2_y = 0, 0
+            s1_x_y, s2_x_y = 0, 0
             if i - 1 >= 0:
-                s_x = s[i - 1][j]
+                s1_x = s1[i - 1][j]
+                s2_x = s2[i - 1][j]
             if j - 1 >= 0:
-                s_y = s[i][j - 1]
+                s1_y = s1[i][j - 1]
+                s2_y = s2[i][j - 1]
             if i - 1 >= 0 and j - 1 >= 0:
-                s_x_y = s[i - 1][j - 1]
-            s[i][j] = image[i][j] + s_x + s_y - s_x_y
-    return s
+                s1_x_y = s1[i - 1][j - 1]
+                s2_x_y = s2[i - 1][j - 1]
+            current_pixel = int(image[i][j])
+            s1[i][j] = current_pixel + s1_x + s1_y - s1_x_y
+            s2[i][j] = current_pixel * current_pixel + s2_x + s2_y - s2_x_y
+    return s1, s2
+
+
+def calculate_sum(s, i1, j1, i2, j2):
+    s_i = 0
+    s_j = 0
+    s_i_j = 0
+    if i1 - 1 >= 0:
+        s_i = s[i1 - 1][j2]
+    if j1 - 1 >= 0:
+        s_j = s[i2][j1 - 1]
+    if i1 - 1 >= 0 and j1 - 1 >= 0:
+        s_i_j = s[i1 - 1][j1 - 1]
+    return s_i_j + s[i2][j2] - s_i - s_j
+
+
+def threshold(image, thresh, i1, j1, i2, j2):
+    for i in range(i1, i2):
+        for j in range(j1, j2):
+            if image[i][j] >= thresh:
+                image[i][j] = 255
+            else:
+                image[i][j] = 0
+
+
+def show_image(*images):
+    for i, image in enumerate(images):
+        cv2.namedWindow(str(i), cv2.WINDOW_NORMAL)
+        cv2.imshow(str(i), image)
+    while True:
+        key = cv2.waitKey(0)
+        if key == 27:
+            cv2.destroyAllWindows()
+            break
 
 
 def main():
     r = 128
-    k = 0.4
-    w = 20
-
-    bins_count = 256
+    k = 0.2
+    w = 25
 
     original_image = cv2.imread('text.png')
 
     grayscale_image = cv2.cvtColor(original_image, cv2.COLOR_BGR2GRAY)
 
+    height, width = grayscale_image.shape
 
+    s1, s2 = calculate_s(grayscale_image)
+
+    for i in range(0, height, w):
+        for j in range(0, width, w):
+            n, m = w, w
+
+            if i + w > height:
+                i2 = height
+                n = height - i
+            else:
+                i2 = i + w
+            if j + w > width:
+                j2 = width
+                m = width - j
+            else:
+                j2 = j + w
+
+            mean = calculate_sum(s1, i, j, i2 - 1, j2 - 1) / n / m
+
+            variance = calculate_sum(s2, i, j, i2 - 1, j2 - 1) / n / m - mean * mean
+
+            t = mean * (1 + k * (math.sqrt(variance) / r - 1))
+
+            threshold(grayscale_image, t, i, j, i2, j2)
+
+    show_image(grayscale_image)
 
 
 main()
