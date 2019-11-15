6b27978659cfd9c926aa57dbf164b59dad675b57
am502 <antegzeperob@gmail.com>
2019-11-15 19:34:34 +0300

    changed segment

--- first_lesson/segment.py
+++ first_lesson/segment.py
@@ -2,39 +2,39 @@ import cv2
 import numpy as np
 
 
-def show_image(window_name, image):
-    cv2.namedWindow(window_name, cv2.WINDOW_NORMAL)
-    cv2.imshow(window_name, image)
+def show_image(*images):
+    for i, image in enumerate(images):
+        cv2.namedWindow(str(i), cv2.WINDOW_NORMAL)
+        cv2.imshow(str(i), image)
+    while True:
+        key = cv2.waitKey(0)
+        if key == 27:
+            cv2.destroyAllWindows()
+            break
 
 
-orig_image = cv2.imread('segment.jpg', 1)
-hsv_image = cv2.cvtColor(orig_image, cv2.COLOR_BGR2HSV)
-show_image('Orig image', orig_image)
-show_image('Hsv image', hsv_image)
+def main():
+    orig_image = cv2.imread('segment.jpg')
+    hsv_image = cv2.cvtColor(orig_image, cv2.COLOR_BGR2HSV)
 
-approximation = 10
+    approximation = 10
 
-low = np.array([30 - approximation, 100, 100])
-high = np.array([30 + approximation, 255, 255])
-mask = cv2.inRange(hsv_image, low, high)
+    low = np.array([30 - approximation, 100, 100])
+    high = np.array([30 + approximation, 255, 255])
 
-hsv_image[mask > 0] = ([0, 0, 0])
-hsv_image[mask == 0] = ([255, 255, 255])
-hsv_image = cv2.GaussianBlur(hsv_image, (49, 49), 0)
-show_image('Hsv with mask', hsv_image)
+    hsv_mask = 255 - cv2.inRange(hsv_image, low, high)
 
-temp_image = cv2.cvtColor(hsv_image, cv2.COLOR_HSV2BGR)
-gray_image = cv2.cvtColor(temp_image, cv2.COLOR_BGR2GRAY)
+    min_square = 4000
 
-contours, hierarchy = cv2.findContours(gray_image, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-for contour in contours:
-    x, y, w, h = cv2.boundingRect(contour)
-    cv2.rectangle(orig_image, (x, y), (x + w, y + h), (0, 0, 255), 3)
+    processed_img = orig_image.copy()
 
-show_image('Orig image with boxes', orig_image)
+    contours, _ = cv2.findContours(hsv_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    for contour in contours:
+        x, y, w, h = cv2.boundingRect(contour)
+        if h * w > min_square:
+            cv2.rectangle(processed_img, (x, y), (x + w, y + h), (0, 0, 255), 3)
 
-while True:
-    k = cv2.waitKey(0)
-    if k == 27:
-        cv2.destroyAllWindows()
-        break
+    show_image(orig_image, hsv_image, hsv_mask, processed_img)
+
+
+main()
