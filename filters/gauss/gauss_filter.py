c6994bc1448d2e178a729323a5e7ec712280bc32
am502 <antegzeperob@gmail.com>
2019-11-20 14:21:38 +0300

    moved files, changed gauss filter

--- filters/gauss/gauss_filter.py
+++ filters/gauss/gauss_filter.py
@@ -4,24 +4,24 @@ import cv2
 import numpy as np
 
 
-class GaussianFilter:
+class GaussFilter:
     def __init__(self, image, radius, sigma):
         self.image = image
         self.height, self.width = image.shape
         self.radius = radius
         self.border = int(radius / 2)
         self.sigma = sigma
-        self.kernel = self.gaussian_kernel()
+        self.kernel = self.gauss_kernel()
 
-    def gaussian_kernel(self):
+    def gauss_kernel(self):
         kernel = np.zeros((self.radius, self.radius), dtype=float)
         for i in range(-self.border, self.border + 1):
             for j in range(-self.border, self.border + 1):
-                kernel[i + self.border][j + self.border] = self.gaussian_function(i, j)
+                kernel[i + self.border][j + self.border] = self.gauss_function(i, j)
         return kernel / kernel.sum()
 
-    def gaussian_function(self, x, y):
-        return 1 / math.sqrt(2 * math.pi) / self.sigma * math.e ** ((-x * x - y * y) / 2 / self.sigma / self.sigma)
+    def gauss_function(self, x, y):
+        return math.e ** ((-x * x - y * y) / 2 / self.sigma / self.sigma) / 2 / math.pi / self.sigma / self.sigma
 
     def filter(self):
         filtered_image = self.image.copy()
@@ -40,9 +40,9 @@ class GaussianFilter:
                 if j < 0:
                     current_j = -j - 1
                 if i >= self.height:
-                    current_i = self.height - (i - self.height) - 2
+                    current_i = self.height - (i - self.height) - 1
                 if j >= self.width:
-                    current_j = self.width - (j - self.width) - 2
+                    current_j = self.width - (j - self.width) - 1
                 result_value += (self.image[current_i][current_j]
                                  * self.kernel[i + self.border - row_index][j + self.border - column_index])
         return result_value
@@ -63,12 +63,9 @@ def calculate_radius_by_sigma(sigma):
     return round(sigma * 6) + 1
 
 
-original_image = cv2.imread('tiger.png')
-hsv = cv2.cvtColor(original_image, cv2.COLOR_BGR2HSV)
-_, _, v = cv2.split(hsv)
-g_filter = GaussianFilter(v, 5, 0.66)
-print(g_filter.kernel)
-print(g_filter.kernel.sum())
-ii = g_filter.filter()
-print(ii.shape)
-show_image(ii, cv2.imread('pt.png'), original_image)
+s = 0.66
+original_image = cv2.imread('tiger.png', 0)
+
+gauss_filter = GaussFilter(original_image, calculate_radius_by_sigma(s), s)
+
+show_image(original_image, gauss_filter.filter())
---
+++ filters/gauss/gauss_filter_main.py
@@ -0,0 +1,18 @@
+import cv2
+
+from filters.gauss.gauss_filter import GaussFilter
+from filters.gauss.gauss_filter import calculate_radius_by_sigma
+from filters.gauss.gauss_filter import show_image
+
+s = 0.66
+original_image = cv2.imread('some.png')
+
+hsv_image = cv2.cvtColor(original_image, cv2.COLOR_BGR2HSV)
+h_plane, s_plane, v_plane = cv2.split(hsv_image)
+
+gauss_filter = GaussFilter(v_plane, calculate_radius_by_sigma(s), s)
+filtered_gray_image = gauss_filter.filter()
+
+filtered_image = cv2.merge([h_plane, s_plane, filtered_gray_image])
+
+show_image(original_image, filtered_image)
